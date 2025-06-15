c124cd2e65bd3b60a2e1b4f69674c800bee58a97
Zuriefais <zuriefais@gmail.com>
2025-06-15 16:07:19 +0300

    Add comments to lexer

--- crates/kumir_lang/src/lib.rs
+++ crates/kumir_lang/src/lib.rs
@@ -5,6 +5,7 @@ pub enum Token {
     Number(i32),
     Operator(String),
     Delimiter(char),
+    String(String),
     Eof,
 }
 
@@ -41,13 +42,27 @@ impl Lexer {
         }
     }
 
+    pub fn skip_string(&mut self) {
+        while let Some(c) = self.current_char {
+            if c == '\n' {
+                // Предполагаем, что комментарий заканчивается на новой строке
+                self.advance();
+                break;
+            }
+            self.advance();
+        }
+    }
+
     pub fn next_token(&mut self) -> Result<Token, String> {
         self.skip_whitespace();
 
         match self.current_char {
             None => Ok(Token::Eof),
             Some(c) => {
-                if c.is_alphabetic() || c == '_' {
+                if '|' == c {
+                    self.skip_string();
+                    self.next_token()
+                } else if c.is_alphabetic() || c == '_' {
                     let word = self.collect_word();
                     if ["алг", "нач", "кон", "цел", "вещ"].contains(&word.as_str()) {
                         Ok(Token::Keyword(word))
@@ -56,19 +71,42 @@ impl Lexer {
                     }
                 } else if c.is_digit(10) {
                     Ok(Token::Number(self.collect_number()))
-                } else if "+-*/=><:".contains(c) {
+                } else if "+-*/=><".contains(c) {
                     let op = c.to_string();
                     self.advance();
                     Ok(Token::Operator(op))
+                } else if c == ':' {
+                    self.advance();
+                    if self.current_char == Some('=') {
+                        self.advance();
+                        Ok(Token::Operator(":=".to_string()))
+                    } else {
+                        Ok(Token::Operator(":".to_string()))
+                    }
                 } else if ".,;()".contains(c) {
                     let delim = c;
                     self.advance();
                     Ok(Token::Delimiter(delim))
+                } else if '"' == c {
+                    self.advance();
+                    Ok(Token::String(self.collect_string()))
                 } else {
                     Err(format!("Неизвестный символ: {}", c))
                 }
             }
         }
+    }
+
+    pub fn collect_string(&mut self) -> String {
+        let mut str = String::new();
+        while self
+            .current_char
+            .map_or(false, |c| c.is_alphanumeric() && c != '"')
+        {
+            str.push(self.current_char.unwrap());
+            self.advance();
+        }
+        str
     }
 
     pub fn collect_word(&mut self) -> String {
--- crates/kumir_lang/src/main.rs
+++ crates/kumir_lang/src/main.rs
@@ -1,23 +1,7 @@
 use kumir_lang::{Lexer, Token};
 
 fn main() {
-    let code = "алг
-    нач
-      вещ число
-      цел целое, сотые
-      лит запись
-      число := 3.14
-      тест(целое, сотые, запись, число)
-    кон
-
-    алг тест (рез цел m, n, лит т, арг вещ y)
-    нач
-      вещ r
-      m := int(y)
-      r := (y - m)*100
-      n := int(r)
-      т := вещ_в_лит(y)
-    кон";
+    let code = include_str!("test.kum");
     let mut lexer = Lexer::new(code);
     loop {
         match lexer.next_token() {
